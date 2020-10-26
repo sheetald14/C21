f4ba3bd7966ccd12822ba35d00ee867e491e3153
sheetald14 <71922963+sheetald14@users.noreply.github.com>
2020-10-26 15:01:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var movingRect,fixedRect;
+var gameObject1,gameObject2,gameObject3,gameObject4;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(400, 200, 50, 80);
+  fixedRect.shapeColor="green";
+  movingRect=createSprite(500,200,80,30);
+  movingRect.shapeColor="green";
+  gameObject1=createSprite(100,100,50,50);
+  gameObject1.shapeColor="green";
+
+  gameObject2=createSprite(200,100,50,50);
+  gameObject2.shapeColor="green";
+  
+  gameObject3=createSprite(300,100,50,50);
+  gameObject3.shapeColor="green";
+  
+  gameObject4=createSprite(400,100,50,50);
+  gameObject4.shapeColor="green";
+
+}
+
+function draw() {
+  background(255,255,255); 
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY; 
+  if(checkTouching(movingRect,gameObject1)){
+    movingRect.shapeColor="red";
+  }else{
+    movingRect.shapeColor="green";
+  }
+  
+ 
+  drawSprites();
+}
+
+function checkTouching(object1,object2){
+  if(object1.x-object2.x < object1.width/2+object2.width/2 && 
+    object2.x-object1.x < object1.width/2+object2.width/2 &&
+    object1.y-object2.y < object1.height/2+object2.height/2 && 
+    object2.y-object1.y < object1.height/2+object2.height/2 ){
+    return true;
+  }
+  else{
+    return false;
+  }
+  
+}
+
+
